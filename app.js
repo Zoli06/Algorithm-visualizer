69eeb5ce02bf406543280fe2be94ac335c30b76f
Zoli06 <63035911+Zoli06@users.noreply.github.com>
2022-03-21 13:07:11 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let numbers = [];
+let numbersLength = 20;
+
+function generateRandomNumbers() {
+    for (let i = 0; i < numbersLength; i++) {
+        numbers.push(Math.random());
+    }
+}
+
+function drawNumbers() {
+}
